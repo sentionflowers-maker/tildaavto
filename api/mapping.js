18c8813704cf009dbf1d796d5530044962e2f3ba
Anna Volkova <annavolkova@Annas-MacBook-Air.local>
2026-02-04 17:58:18 +0500

    Add Amsterdam Pie mapping

--- api/mapping.js
+++ api/mapping.js
@@ -25,6 +25,14 @@ module.exports = [
   },
   {
     city: 'msk',
+    tildaName: 'Амстердамский пирог',
+    iikoProductId: 'a80e6ecb-3479-47ba-a221-d1dde5e7b8f3',
+    type: 'Compound',
+    modifierSchemaId: '0d0d70f2-ce97-4f83-9ff4-0a49cc31029b', // Размер
+    sizeId: '7c23e0c5-ef2e-4a16-8653-15918a7807d7' // 24 см (стандарт)
+  },
+  {
+    city: 'msk',
     tildaName: 'Вкусное путешествие',
     iikoProductId: 'ea7586e1-f6bc-49cf-b7e5-8ee7be75f1ba'
   },
